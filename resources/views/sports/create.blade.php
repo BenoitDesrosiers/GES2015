4abba69e64305813c731340761b9b28233258a04
benoitdesrosiers <desrosib@cegepdrummond.ca>
2015-07-14 16:29:55 -0400

    j'avais oublié de changer les {{ dans sports/create

--- resources/views/sports/create.blade.php
+++ resources/views/sports/create.blade.php
@@ -5,43 +5,43 @@
 		<h2>Création d'un sport</h2>
 	</div>
 	<div class="panel-body">
-		{{ Form::open(['action'=> 'SportsController@index', 'class' => 'form']) }}
+		{!! Form::open(['action'=> 'SportsController@index', 'class' => 'form']) !!}
 		<div class="form-group">
-			{{ Form::label('nom', 'Nom:') }} 
-			{{ Form::text('nom',null, ['class' => 'form-control']) }}
+			{!! Form::label('nom', 'Nom:') !!} 
+			{!! Form::text('nom',null, ['class' => 'form-control']) !!}
 			{{ $errors->first('nom') }}
 		</div>
 		<div class="form-group">
-			{{ Form::label('saison', 'Saison:') }} 
-			{{ Form::radio('saison','h', true, ['id'=>'hiver', 'class' => 'radio-inline']) }} Hiver
-			{{ Form::radio('saison','e', false, ['id'=>'ete', 'class' => 'radio-inline']) }} Été
+			{!! Form::label('saison', 'Saison:') !!} 
+			{!! Form::radio('saison','h', true, ['id'=>'hiver', 'class' => 'radio-inline']) !!} Hiver
+			{!! Form::radio('saison','e', false, ['id'=>'ete', 'class' => 'radio-inline']) !!} Été
 			{{ $errors->first('saison') }}				
 		</div>
 		<div class="form-group">
-			{{ Form::label('description_courte', 'Description courte:') }} 
-			{{ Form::text('description_courte',null, ['class' => 'form-control']) }}
+			{!! Form::label('description_courte', 'Description courte:') !!} 
+			{!! Form::text('description_courte',null, ['class' => 'form-control']) !!}
 			{{ $errors->first('description_courte') }}				
 		</div>
 		<div class="form-group">
-			{{ Form::label('url_logo', 'Url du logo:') }} 
-			{{ Form::text('url_logo',null, ['class' => 'form-control']) }}
+			{!! Form::label('url_logo', 'Url du logo:') !!} 
+			{!! Form::text('url_logo',null, ['class' => 'form-control']) !!}
 			{{ $errors->first('url_logo') }}							
 		</div>	
 		<div class="form-group">
-			{{ Form::label('url_page_officielle', 'Url de la page officielle:') }} 
-			{{ Form::text('url_page_officielle',null, ['class' => 'form-control']) }}
+			{!! Form::label('url_page_officielle', 'Url de la page officielle:') !!} 
+			{!! Form::text('url_page_officielle',null, ['class' => 'form-control']) !!}
 			{{ $errors->first('url_page_officielle') }}				
 		</div>	
 		<div class="form-group">
-			{{ Form::label('tournoi', 'Tournoi:') }} 
-			{{ Form::checkbox('tournoi',null, ['class' => 'form-control']) }}
+			{!! Form::label('tournoi', 'Tournoi:') !!} 
+			{!! Form::checkbox('tournoi',null, ['class' => 'form-control']) !!}
 			{{ $errors->first('tournoi') }}
 		</div>	
 		<div class="form-group">
-			{{ Form::button('Créer', ['type' => 'submit', 'class' => 'btn btn-primary']) }}
+			{!! Form::button('Créer', ['type' => 'submit', 'class' => 'btn btn-primary']) !!}
 			<a href="{{ URL::previous() }}" class="btn btn-danger">Annuler</a>
 		</div>
-		{{ Form::close() }}
+		{!! Form::close() !!}
 	</div>
 </div>
 @stop
