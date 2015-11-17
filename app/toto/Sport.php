897ce50521c4791efb30b1f5a278af8a86dbb2bd
Sarah Laflamme <sarahtheflame@gmail.com>
2015-11-17 15:58:30 -0500

    bouette

---
+++ app/toto/Sport.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * La classe Sport
+ * 
+ * 
+ * @author benou
+ * @version 0.1
+ */
+
+namespace App\Models;
+
+class Sport extends EloquentValidating {
+	protected $guarded = array('id');
+
+
+	/**
+	 * Eloquent relationship: un sport a plusieurs épreuves
+	 */ 
+	
+	public function epreuves() {
+		return $this->hasMany('App\Models\Epreuve');
+	}
+	
+	public function participants() {
+		return $this->belongsToMany('App\Models\Participant');
+	}
+	
+
+/**
+ * Validation
+ *
+ * un sport doit avoir:
+ * - nom: obligatoire, et unique dans toute la table
+ * - saison : obligatoire, e (été), ou h (hiver)
+ * - tournoi : obligatoire, booléen
+ * - Les autres champs sont falcultatifs.
+ */
+
+public function validationRules() {
+	return [
+		'nom' => 'required|unique:sports,nom'.($this->id ? ",$this->id" : ''),
+ 		'saison' => 'in:e,h',
+		'tournoi' => 'required|boolean'
+		];
+}
+
+}
