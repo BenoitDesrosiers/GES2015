187321256cd6ab1863a13a9cf7a9379116f09a00
obnosim <obnosim@gmail.com>
2015-11-24 14:26:36 -0500

    Laisser une équipe vide dans le seeder pour la démonstration

--- database/seeds/EquipesSeeder.php
+++ database/seeds/EquipesSeeder.php
@@ -22,7 +22,7 @@ public function creerEquipes() {
 		["Grizzlis effarouchés"			, 1,	1,	1,	[ 1, 2, 3]],
 		["Opossums déchaînés"			, 2,	1,	2,	[ 4, 5, 6]],
 		["Antilopes frustrées"			, 3,	1,	3,	[ 7, 8, 9]],
-		["Nasiques offensés"			, 4,	2,	1,	[10,11,12]],
+		["Nasiques offensés"			, 4,	2,	1,	[]],
 		["Renards désinhibés"			, 5,	2,	1,	[13,14,15]],
 		["Escargots enragés"			, 6,	2,	3,	[16,18,19]],
 		["Méduses surexcitées"			, 7,	3,	2,	[20,21,22]],
