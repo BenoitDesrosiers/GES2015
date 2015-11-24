d1c58655babe724cc3b2833b8433ba698a6aebeb
benoitdesrosiers <desrosib@cegepdrummond.ca>
2015-11-23 22:38:01 -0500

    ajouter namespace pour Systeme

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\BaseController;
 use View;
-use Systeme;
+use App\Models\Systeme;
 /**
  * Le controller principal
  * 
--- app/Http/Controllers/SystemeController.php
+++ app/Http/Controllers/SystemeController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use View;
-use Systeme;
+use App\Models\Systeme;
 use Input;
 use Redirect;
 
