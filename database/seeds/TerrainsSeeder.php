5ffd3042e61fafa23498ba90c7713c75e1ceb748
IMFUZZ <da.junior.du@gmail.com>
2015-12-01 14:32:09 -0500

    Seeder de terrain sécurisé

--- database/seeds/TerrainsSeeder.php
+++ database/seeds/TerrainsSeeder.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 
 use App\Models\Terrain;
 use App\Models\Region;
+use App\Models\Sport;
 
 class TerrainsTableSeeder extends Seeder {
 
@@ -14,24 +15,27 @@ class TerrainsTableSeeder extends Seeder {
         // Suppression de la base de données
         DB::table('terrains')->delete();
 
-        // Création de la variable 'terrain' qui correspond à la 
+        $regions = DB::table('regions')->get();
+        $sports = DB::table('sports')->get();
+
+        // Création de la variable 'items' qui correspond à la 
         // liste des champs à 'seeder' dans la base de données
-        $terrains = [
-        ["Football 1 Cégep de Drummondville",   "960 Rue Saint-Georges",    "Drummondville", "4"],
-        ["Football 2 Cégep de Drummondville",   "960 Rue Saint-Georges",    "Drummondville", "4"],
-        ["Football 3 Cégep de Drummondville",   "960 Rue Saint-Georges",    "Drummondville", "4"],
-        ["Soccer 1 Cégep de Drummondville",     "960 Rue Saint-Georges",    "Drummondville", "4"],
-        ["Soccer 2 Cégep de Drummondville",     "960 Rue Saint-Georges",    "Drummondville", "4"],
-        ["Football 1 Marie-Rivier",             "265 Rue Saint Félix",      "Drummondville", "4"],
-        ["Football 2 Marie-Rivier",             "265 Rue Saint Félix",      "Drummondville", "4"],
-        ["Football 3 Marie-Rivier",             "265 Rue Saint Félix",      "Drummondville", "4"],
-        ["Soccer 1 Marie-Rivier",               "265 Rue Saint Félix",      "Drummondville", "4"],
-        ["Soccer 2 Marie-Rivier",               "265 Rue Saint Félix",      "Drummondville", "4"],
-        ["Football 1 Jean-Raimbault",           "175 Rue Pelletier",        "Drummondville", "4"],
-        ["Football 2 Jean-Raimbault",           "175 Rue Pelletier",        "Drummondville", "4"],
-        ["Football 3 Jean-Raimbault",           "175 Rue Pelletier",        "Drummondville", "4"],
-        ["Soccer 1 Jean-Raimbault",             "175 Rue Pelletier",        "Drummondville", "4"],
-        ["Soccer 2 Jean-Raimbault",             "177 Rue Pelletier",        "Drummondville", "4"]
+        $items = [
+        ["Football 1 Cégep de Drummondville",   "960 Rue Saint-Georges",    "Drummondville"],
+        ["Football 2 Cégep de Drummondville",   "960 Rue Saint-Georges",    "Drummondville"],
+        ["Football 3 Cégep de Drummondville",   "960 Rue Saint-Georges",    "Drummondville"],
+        ["Soccer 1 Cégep de Drummondville",     "960 Rue Saint-Georges",    "Drummondville"],
+        ["Soccer 2 Cégep de Drummondville",     "960 Rue Saint-Georges",    "Drummondville"],
+        ["Football 1 Marie-Rivier",             "265 Rue Saint Félix",      "Drummondville"],
+        ["Football 2 Marie-Rivier",             "265 Rue Saint Félix",      "Drummondville"],
+        ["Football 3 Marie-Rivier",             "265 Rue Saint Félix",      "Drummondville"],
+        ["Soccer 1 Marie-Rivier",               "265 Rue Saint Félix",      "Drummondville"],
+        ["Soccer 2 Marie-Rivier",               "265 Rue Saint Félix",      "Drummondville"],
+        ["Football 1 Jean-Raimbault",           "175 Rue Pelletier",        "Drummondville"],
+        ["Football 2 Jean-Raimbault",           "175 Rue Pelletier",        "Drummondville"],
+        ["Football 3 Jean-Raimbault",           "175 Rue Pelletier",        "Drummondville"],
+        ["Soccer 1 Jean-Raimbault",             "175 Rue Pelletier",        "Drummondville"],
+        ["Soccer 2 Jean-Raimbault",             "177 Rue Pelletier",        "Drummondville"]
         ];
 
         //todo: lire les régions à partir de la BD et associer des ids de régions qui existent
@@ -40,14 +44,22 @@ class TerrainsTableSeeder extends Seeder {
         // 		voir ArbitresSeeder pour un exemple de comment le faire.  
          
         // 'seed' de la base de données
-        foreach($terrains as $terrain) {
-            Terrain::create(
-                array(
-                    'nom'=>$terrain[0], 
-                    'adresse'=>$terrain[1], 
-                    'ville'=>$terrain[2],  
-                    'region_id'=>$terrain[3])
-                );
+
+        foreach($items as $item) {
+            $terrain = new Terrain();
+            $terrain->nom = $item[0];
+            $terrain->adresse = $item[1];
+            $terrain->ville = $item[2];
+            $terrain->region_id = rand(1, count($regions));
+            $terrain->save();
+            $index_sports = array();
+            for ($x = 1; $x < rand(1, count($sports)); $x++) {
+                $test = rand(0, 1);
+                if ($test === 1) {
+                    array_push($index_sports, $x);
+                }   
+            }
+            $terrain->sports()->sync($index_sports);
         }
     }
 }
