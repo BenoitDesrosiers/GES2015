712e950d99abbf41920e93d8f60eca6eb4b55dc4
Sarah Laflamme <sarahtheflame@gmail.com>
2015-12-15 16:02:29 -0500

    Correction #2 du seeder

--- database/seeds/ArbitresSeeder.php
+++ database/seeds/ArbitresSeeder.php
@@ -4,34 +4,40 @@ use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
 use App\Models\Arbitre;
+use App\Models\Region;
 
 class ArbitresTableSeeder extends Seeder {
 
-
+	/**
+	 * Création d'arbitres dans la base de données
+	 */
 	public function run()
 	{
 
 		$entrees = [
-		//prénom,	nom				 region, #,  association, 	telephone, 		sexe
-		["Benoit"	, "Desrosiers"	, 1,	 1,		"AQSFR",	"819-578-6489",		0],
-		["Guy"	, "Bernard"	, 2,	 1,		"OSQ",			"450-715-6915",		0],
-		["Jonathan"	, "Gareau"		, 3,	 1,		"ASSQ",			"514-763-2485",		0],
-		["Stéphane", "Janvier"	, 4,	 2,		"ASAQ",			"450-571-1203",		0],
+			//prénom	  nom			numéro      association 	telephone 		  sexe
+			["Benoit" ,   "Desrosiers"	,  "1",		"AQSFR",		"819-578-6489",		0],
+			["Guy"	, 	  "Bernard"	,      "1",		"OSQ",			"450-715-6915",		0],
+			["Jonathan"	, "Gareau" ,  	   "1",		"ASSQ",			"514-763-2485",		0],
+			["Stéphane",  "Janvier" ,	   "2",		"ASAQ",			"450-571-1203",		0]
+		];
+
+		$regions = Region::all();
 
 		foreach($entrees as $entree) {
 			$arbitre = new Arbitre;
 			$arbitre->prenom = $entree[0];
 			$arbitre->nom = $entree[1];
-			$arbitre->region_id = $entree[2];
-			$arbitre->numero_accreditation = $entree[3];
-			$arbitre->association = $entree[4];
-			$arbitre->numero_telephone = '819-123-4567';
-			$arbitre->sexe = rand(0,1);
-			$date_temp = new DateTime;
-	        $date_temp->setDate(1994, 1, 1);
-	        $arbitre->date_naissance=$date_temp;
-			
-			$arbitre->save();
+			$arbitre->region_id = $regions->random()->id;
+			$arbitre->numero_accreditation = $entree[2];
+			$arbitre->association = $entree[3];
+			$arbitre->numero_telephone = $entree[4];
+			$arbitre->sexe = $entree[5];
+			if ($arbitre->save()) {
+
+			} else {
+				echo("save non réussi");
+			}
 		}
 	}
 }
