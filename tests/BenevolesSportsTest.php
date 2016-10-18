f141c53cb0bb4627684aaf71a0fd35af3549bf57
steveduquet <steve.duquet@outlook.com>
2016-10-18 11:10:53 -0400

    Création d'un fichier de tests. Test de création du modèle BenevolesSports.

---
+++ tests/BenevolesSportsTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class BenevolesSportsTest extends TestCase
+{
+    /** @test */
+    public function la_creation_d_un_benevole_sport_fonctionne()
+    {
+    	$benevolesSports= factory(App\Models\BenevolesSports::class)
+    	->make(['benevole_id'=>'1', 'sport_id'=>'2']);
+        $this->assertEquals('1', $voiture->benevole_id);
+        $this->assertEquals('2', $voiture->sport_id);
+    }
+}
