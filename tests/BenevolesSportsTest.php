d66b1f9111ff28dd0ee4144dadf13081f9cb4d2d
steveduquet <steve.duquet@outlook.com>
2016-10-18 11:28:52 -0400

    Ajout de la migration benevoles_sports_table.

--- tests/BenevolesSportsTest.php
+++ tests/BenevolesSportsTest.php
@@ -14,4 +14,11 @@ class BenevolesSportsTest extends TestCase
         $this->assertEquals('1', $voiture->benevole_id);
         $this->assertEquals('2', $voiture->sport_id);
     }
+    
+    /** @test */
+    public function la_creation_d_un_benevole_sport_dans_la_BD()
+    {
+    	$benevolesSports = factory(App\Models\BenevolesSports::class)
+    	->create();
+    }
 }
