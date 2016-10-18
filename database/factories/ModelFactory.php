981b81117c5bf27d6c5ee8c24d3f43898f5c0127
Caya <6105160@cegepdrummond.ca>
2016-10-18 13:02:52 -0400

    branche steve

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,11 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Models\Taches::class, function (Faker\Generator $faker) {
+    return [
+    	'identifiant' => rand(1,1000),
+    	'nom' => $faker->name,
+        'description' => $faker->email
+        ];
+});
---
+++ database/migrations/2016_10_18_110859_create_taches_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTachesTable extends Migration
+{
+
+    public function up()
+    {
+         Schema::create('taches', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nom');
+            $table->string('description');
+        });             
+    }
+    public function down()
+    {
+        //        
+        Schema::drop('taches');  
+    }
+}
