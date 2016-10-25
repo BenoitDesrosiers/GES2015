fe538a30d36f083be7dc25c3dd7100e76b62723a
Nicolas Bisson <niclas035@hotmail.com>
2016-10-25 10:44:28 -0400

    Modification vers syntaxe blade

--- resources/views/epreuves/ajtParticipant.blade.php
+++ resources/views/epreuves/ajtParticipant.blade.php
@@ -34,24 +34,21 @@
             </div>
             <!-- Fin de mon code ici -->
 			<div>
-				<?php  //TODO: séparer les participants par régions, ou encore mieux, avoir un filtre de région
-					foreach ($participants as $participant) {
-						$checked = " ";
-						//FIXME: se servir des fonctions de collections: $listeIds = $epreuveParticipants->pluck('id'); ....  if($listeIds->contains($participant->id))...
-						foreach ($epreuveParticipants as $epreuvePart) {
-							if ($epreuvePart->id == $participant->id) {
-								$checked = " checked";
-								continue;
-							}
-						}
-                    $region = $participant->region;
-				?>
+                <!-- TODO: séparer les participants par régions, ou encore mieux, avoir un filtre de région -->
+					@foreach ($participants as $participant)
+                        <?php $checked = " "; ?>
+                        <!-- FIXME: se servir des fonctions de collections: $listeIds = $epreuveParticipants->pluck('id'); ....  if($listeIds->contains($participant->id))... -->
+                        @foreach ($epreuveParticipants as $epreuvePart)
+							@if ($epreuvePart->id == $participant->id)
+                                <?php $checked = " checked"; ?>
+                            @endif
+						@endforeach	
+
+                    <?php $region = $participant->region; ?>
 				<div>
 			            <input type="checkbox" name="participants[{{$participant->id}}]" <?=$checked?>> {{$participant->id}} {{$participant->nom}}, {{$participant->prenom}} {{$region->nom}} <br/> <!-- Ajout d'infos ici -->
 			    </div>
-				<?php
-					}
-				?>
+                    @endforeach
 			</div>
 			
 		<div class="form-group">
