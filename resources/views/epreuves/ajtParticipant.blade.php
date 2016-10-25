7749618ca4270d7c56d16b53db3e8c4623e4fc19
Nicolas Bisson <niclas035@hotmail.com>
2016-10-25 09:47:30 -0400

    Ajout d'une ligne de titre

--- resources/views/epreuves/ajtParticipant.blade.php
+++ resources/views/epreuves/ajtParticipant.blade.php
@@ -23,11 +23,23 @@
 				{!! Form::select('region_id', $regionArray) !!}
 				{{ $errors->first('region_id') }}
 			</div>
+            <div>
+                <table class="table table-striped table-hover">
+                    <tr>
+                        <th >Numéro</th>
+                        <th>Participant</th>
+                        <th>Région</th>
+                    </tr>
+                </table>
+            </div>
             <!-- Fin de mon code ici -->
 			<div>
 				<?php  //TODO: séparer les participants par régions, ou encore mieux, avoir un filtre de région
+                    // Début de mon code ici
+
+                    // Fin de mon code ici
 					foreach ($participants as $participant) {
-						$checked = "";
+						$checked = " ";
 						//FIXME: se servir des fonctions de collections: $listeIds = $epreuveParticipants->pluck('id'); ....  if($listeIds->contains($participant->id))... 
 						foreach ($epreuveParticipants as $epreuvePart) {
 							if ($epreuvePart->id == $participant->id) {
