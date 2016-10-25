b36eb134c88322d6abb311417dcdd486ad33cfc1
Nicolas Bisson <niclas035@hotmail.com>
2016-10-25 10:55:07 -0400

    Ajout colonne vide et petit formatage code

--- resources/views/epreuves/ajtParticipant.blade.php
+++ resources/views/epreuves/ajtParticipant.blade.php
@@ -5,65 +5,59 @@
 		<h2>Ajouter un participant : {{$epreuve->nom}}</h2>
 	</div>
 	
-<table class="table table-striped table-hover">
-	<tbody>
+    <table class="table table-striped table-hover">
+        <tbody>
 
-		{!! Form::open(array('action' => array('EpreuvesController@storeParticipants', $epreuve->id))) !!}
+            {!! Form::open(array('action' => array('EpreuvesController@storeParticipants', $epreuve->id))) !!}
 
-			<!--    Méthode comme délégué create
-                    Début mon code ici              -->
-			<?php
-			$regionArray = array();
-			for ($i=0; $i<count($regions); $i++) {
-				$regionArray[$regions[$i]['id']]  = $regions[$i]['nom'];
-			}
-			?>
-			<div class="form-group">
-				{!! Form::label('region_id', 'Région:') !!} <br/>
-				{!! Form::select('region_id', $regionArray) !!}
-				{{ $errors->first('region_id') }}
-			</div>
-            <div>
-                <table class="table table-striped table-hover">
-                    <tr>
-                        <th >Numéro</th>
-                        <th>Participant</th>
-                        <th>Région</th>
-                    </tr>
-                </table>
+                <!-- Début mon code ici -->
+                <?php
+                $regionArray = array();
+                for ($i=0; $i<count($regions); $i++) {
+                    $regionArray[$regions[$i]['id']]  = $regions[$i]['nom'];
+                }
+                ?>
+                <div class="form-group">
+                    {!! Form::label('region_id', 'Région:') !!} <br/>
+                    {!! Form::select('region_id', $regionArray) !!}
+                    {{ $errors->first('region_id') }}
+                </div>
+                <div>
+                    <table class="table table-striped table-hover">
+                        <tr>
+                            <th>&nbsp;</th>
+                            <th>Numéro</th>
+                            <th>Participant</th>
+                            <th>Région</th>
+                        </tr>
+                    </table>
+                </div>
+                <!-- Fin de mon code ici -->
+                <div>
+                    <!-- TODO: séparer les participants par régions, ou encore mieux, avoir un filtre de région -->
+                        <!-- Début modification syntaxe ici (pour blade) -->
+                        @foreach ($participants as $participant)
+                            <?php $checked = " "; ?>
+                            <!-- FIXME: se servir des fonctions de collections: $listeIds = $epreuveParticipants->pluck('id'); ....  if($listeIds->contains($participant->id))... -->
+                            @foreach ($epreuveParticipants as $epreuvePart)
+                                @if ($epreuvePart->id == $participant->id)
+                                    <?php $checked = " checked"; ?>
+                                @endif
+                            @endforeach
+                        <?php $region = $participant->region; ?>
+                    <div>
+                        <input type="checkbox" name="participants[{{$participant->id}}]" <?=$checked?>> {{$participant->id}} {{$participant->nom}}, {{$participant->prenom}} {{$region->nom}} <br/> <!-- Ajout d'infos ici -->
+                    </div>
+                        @endforeach
+                </div>
+                <!-- Fin modification syntaxe ici (pour blade) -->
+            <div class="form-group">
+                {!! Form::submit('Appliquer', array('class' => 'btn btn-primary')) !!}
+                <a href="{{ action('EpreuvesController@index') }}" class="btn btn-danger">Annuler</a>
             </div>
-            <!-- Fin de mon code ici -->
-			<div>
-                <!-- TODO: séparer les participants par régions, ou encore mieux, avoir un filtre de région -->
-					@foreach ($participants as $participant)
-                        <?php $checked = " "; ?>
-                        <!-- FIXME: se servir des fonctions de collections: $listeIds = $epreuveParticipants->pluck('id'); ....  if($listeIds->contains($participant->id))... -->
-                        @foreach ($epreuveParticipants as $epreuvePart)
-							@if ($epreuvePart->id == $participant->id)
-                                <?php $checked = " checked"; ?>
-                            @endif
-						@endforeach	
-
-                    <?php $region = $participant->region; ?>
-				<div>
-			            <input type="checkbox" name="participants[{{$participant->id}}]" <?=$checked?>> {{$participant->id}} {{$participant->nom}}, {{$participant->prenom}} {{$region->nom}} <br/> <!-- Ajout d'infos ici -->
-			    </div>
-                    @endforeach
-			</div>
-			
-		<div class="form-group">
-			{!! Form::submit('Appliquer', array('class' => 'btn btn-primary')) !!}
-			<a href="{{ action('EpreuvesController@index') }}" class="btn btn-danger">Annuler</a>
-		</div>
-		{!! Form::close() !!}
-		{!! Form::close() !!}
-	</tbody>
-</table>
-<script>
-	function afficherListeRegions() {
-		$.ajax({
-
-		});
-	}
-</script>
+            {!! Form::close() !!}
+            {!! Form::close() !!}
+        </tbody>
+    </table>
+</div>
 @stop
