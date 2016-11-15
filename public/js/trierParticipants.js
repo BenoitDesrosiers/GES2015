5ac043785a57cd2d4663e3d657bceece39d345aa
Nicolas Bisson <niclas035@hotmail.com>
2016-11-15 09:23:32 -0500

    Reception des usagers pour les afficher. N'affiche rien...

--- public/js/trierParticipants.js
+++ public/js/trierParticipants.js
@@ -18,4 +18,67 @@ $('#region_id').change(function() {
             }
         }
     }
+
+    listerParticipants(participants_region);
 });
+
+/**
+ * Crée la table qui sert à lister les participants.
+ * Affiche les participants dans cette table.
+ */
+function listerParticipants(participants_region) {
+    var table = document.createElement('table');
+    console.log("allo");
+    if (participants_region.length < 1) {
+        console.log("allo0");
+        var texte = document.createElement('h4');
+        texte.innerHTML = "Il n'y a pas de participant inscrit pour cette région.";
+        table.appendChild(texte)
+    } else {
+        console.log("allo1");
+        var corpsTable = document.createElement('tbody');
+        var titre = document.createElement('tr');
+        var titreNumero = document.createElement('th');
+        var titreParticipant = document.createElement('th');
+        var titreRegion = document.createElement('th');
+        var colonneCheckbox = document.createElement('th');
+
+        table.className = 'table table-striped table-hover';
+        titreNumero.innerHTML = 'Numéro';
+        titreParticipant.innerHTML = 'Participant';
+        titreRegion.innerHTML = 'Région';
+        colonneCheckbox.innerHTML = ' ';
+
+        titre.appendChild(titreNumero);
+        titre.appendChild(titreParticipant);
+        titre.appendChild(titreRegion);
+        titre.appendChild(colonneCheckbox);
+
+        for (participant in participants_region) {
+            var ligneParticipant = document.createElement('tr');
+            var headerCheckbox = document.createElement('th');
+            var checkbox = document.createElement('input');
+            checkbox.name = 'participant[' + participants_region[participant]["participant_id"] + ']';
+            checkbox.type = 'checkbox';
+
+            headerCheckbox.appendChild(checkbox);
+            ligneParticipant.appendChild(headerCheckbox);
+            ligneParticipant.appendChild(checkbox);
+
+            var numeroParticipant = document.createElement('th');
+            numeroParticipant.innerHTML = participants_region[participant]['numero'];
+            ligneParticipant.appendChild(numeroParticipant);
+
+            var nomParticipant = document.createElement('th');
+            nomParticipant.innerHTML = participants_region[participant]['nom'];
+            ligneParticipant.appendChild(nomParticipant);
+
+            var regionParticipant = document.createElement('th');
+            regionParticipant.innerHTML = participants_region[participant]['region'];
+            ligneParticipant.appendChild(regionParticipant);
+
+            table.appendChild(corpsTable);
+        }
+    }
+
+}
--- resources/views/epreuves/ajtParticipant.blade.php
+++ resources/views/epreuves/ajtParticipant.blade.php
@@ -1,5 +1,7 @@
 ﻿@extends('layout')
 @section('content')
+@section('script')
+
 <div class="panel panel-default">
 	<div class="panel-heading">
 		<h2>Ajouter un participant : {{$epreuve->nom}}</h2>
@@ -7,7 +9,6 @@
 	
     <table class="table table-striped table-hover">
         <tbody>
-
             {!! Form::open(array('action' => array('EpreuvesController@storeParticipants', $epreuve->id))) !!}
                 <!-- Dropdown -->
                 <?php $regionArray = ["Toutes les régions"]; ?>
@@ -19,77 +20,38 @@
                     {!! Form::select('region_id', $regionArray) !!}
                     {{ $errors->first('region_id') }}
                 </div>
-
-                <div>
-                    <!-- Entête tableau -->
-                    <table class="table table-striped table-hover">
-                        <tr>
-                            <th>&nbsp;</th>
-                            <th>Numéro</th>
-                            <th>Participant</th>
-                            <th>Région</th>
-                        </tr>
-                </div>
-                    <div>
-                    <!-- TODO: séparer les participants par régions, ou encore mieux, avoir un filtre de région -->
-                    <tr>
-                        @foreach ($participants as $participant)
-                            <?php $checked = " "; ?>
-                            <!-- FIXME: se servir des fonctions de collections: $listeIds = $epreuveParticipants->pluck('id'); ....  if($listeIds->contains($participant->id))... -->
-                            @foreach ($epreuveParticipants as $epreuvePart)
-                                @if ($epreuvePart->id == $participant->id)
-                                    <?php $checked = " checked"; ?>
-                                @endif
-                            @endforeach
-                        <?php $region = $participant->region; ?>
-                    </tr>
-                        <div>
-                            <!-- Liste des participants -->
-                            <th>
-                                <input type="checkbox" name="participants[{{$participant->id}}]" <?=$checked?>>
-                            </th>
-                            <script>
-
-                            </script>
-                            <th>{{$participant->numero}}</th>
-                            <th>{{$participant->nom}}, {{$participant->prenom}}</th>
-                            <th>{{$region->nom}}</th>
-                            <br/>
-                            <!--
-                            * if participant->region->id = regionid
-                            *      if participant.isEmpty
-                            *          print aucun participant
-                            *      else
-                            *          print participant
-                            * elseif regionid = 0
-                            *      print participant //tous les participants
-                            -->
-                        </div>
-                        @endforeach
-                    </table>
-                </div>
-            <div class="form-group">
-                {!! Form::submit('Appliquer', array('class' => 'btn btn-primary')) !!}
-                <a href="{{ action('EpreuvesController@index') }}" class="btn btn-danger">Annuler</a>
-            </div>
-                {!! Form::close() !!}
-            {!! Form::close() !!}
+                @foreach ($participants as $participant)
+                    <?php $checked = " "; ?>
+                    <!-- FIXME: se servir des fonctions de collections: $listeIds = $epreuveParticipants->pluck('id'); ....  if($listeIds->contains($participant->id))... -->
+                    @foreach ($epreuveParticipants as $epreuvePart)
+                        @if ($epreuvePart->id == $participant->id)
+                            <?php $checked = " checked"; ?>
+                        @endif
+                    @endforeach
+                <?php $region = $participant->region; ?>
+                @endforeach
         </tbody>
     </table>
+    <div class="form-group">
+        {!! Form::submit('Appliquer', array('class' => 'btn btn-primary')) !!}
+        <a href="{{ action('EpreuvesController@index') }}" class="btn btn-danger">Annuler</a>
+    </div>
+            {!! Form::close() !!}
+        {!! Form::close() !!}
     <script>
         var participants = {
             @foreach($participants as $participant)
                 {{ $participant->id }}: {
+                    // Les variables commencent par des lettres de l'alphabet, car ils se classent par ordre alphabétique.
                     numero: "{{ $participant->numero  }}",
                     nom:"{{ $participant->nom }}, {{ $participant->prenom }}",
+                    region:"{{ $participant->region->nom }}",
                     participant_region_id:"{{ $participant->region->id }}",
-                    region:"{{ $participant->region->nom }}"
+                    participant_id:"{{ $participant->id }}"
                 },
             @endforeach
         };
     </script>
-    <script src="{!! asset('/js/trierParticipants.js') !!}">
-
-    </script>
+    <script src="{!! asset('/js/trierParticipants.js') !!}"></script>
 </div>
 @stop
