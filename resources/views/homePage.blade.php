98e36431b4425c5e2dc736eb5c80f00e405b7b9d
Francis M <franky_1030@hotmail.com>
2016-11-22 18:42:23 -0500

    correction de bug

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -10,16 +10,16 @@
 		<a class="btn btn-lg btn-success" href="{{ action('ParticipantsController@index') }}" role="button">Participants</a>
 		<a class="btn btn-lg btn-success" href="{{ action('EquipesController@index') }}" role="button">Équipes</a></p>
 		<p><a class="btn btn-lg btn-success" href="{{ action('ResultatsController@index') }}" role="button">Résultats</a>
-		<a class="btn btn-lg btn-success" href="{{ action('SystemeController@index') }}" role="button">Système</a></p>
+		<a class="btn btn-lg btn-success" href="{{ action('SystemeController@index') }}" role="button">Système</a>
 		<a class="btn btn-lg btn-success" href="{{ action('BenevolesController@index') }}" role="button">Bénévoles</a>
 		<a class="btn btn-lg btn-success" href="{{ action('TachesController@index') }}" role="button">Tâches</a>
-		<a class="btn btn-lg btn-success" href="{{ action('ArbitresController@index') }}" role="button">Arbitres</a>
+		<a class="btn btn-lg btn-success" href="{{ action('ArbitresController@index') }}" role="button">Arbitres</a></p>
 		<a class="btn btn-lg btn-success" href="{{ action('DeleguesController@index') }}" role="button">Délégués</a>
 		<a class="btn btn-lg btn-success" href="{{ action('RolesController@index') }}" role="button">Rôles</a></p>
 		<a class="btn btn-lg btn-success" href="{{ action('PointagesController@index') }}" role="button">Pointages</a></p>
 		<a class="btn btn-lg btn-success" href="{{ action('TerrainsController@index') }}" role="button">Terrains</a></p>
 		<a class="btn btn-lg btn-success" href="{{ action('EvenementsController@index') }}" role="button">Événements</a></p>
-		<a class="btn btn-lg btn-success" href="{{ action('CodesController@index') }}" role="button">Codes</a></p>	
+		<a class="btn btn-lg btn-success" href="{{ action('CodesController@index') }}" role="button">Codes</a>	</p>
 		<a class="btn btn-lg btn-success" href="{{ action('AboutController@index') }}" role="button">À propos</a></p>
 		<a class="btn btn-lg btn-success" href="{{ action('AssociationsController@index') }}" role="button">Associations</a></p>
 
