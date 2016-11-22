61555bedb4b4ef4207e5edadbaa92bef24727700
ettdro <ett.drolet@hotmail.com>
2016-11-22 09:44:25 -0500

    Correction de la première tâche.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,14 +19,6 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
-
-
-$factory->define(App\Models\Organisme::class, function (Faker\Generator $faker) {
-	return [
-			'nomOrganisme' => $faker->name,
-			'telephone' => $faker->tollFreePhoneNumber,
-			'description' => $faker->sentence
-		];
 		
 $factory->define(App\Models\Participant::class, function (Faker\Generator $faker) {
 	return [
