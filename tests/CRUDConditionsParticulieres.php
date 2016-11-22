ef3ca35f36afa4b61a8349353f8c782298c99eda
Émilio Gonzalez <egg997@gmail.com>
2016-11-22 11:30:52 -0500

    - Création de la classe de test pour le CRUD des conditions particulières.

---
+++ tests/CRUDConditionsParticulieres.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+/**
+ * Class CRUDConditionsParticulieres
+ *
+ * Classe de tests unitaires pour le CRUD des
+ * conditions particulières.
+ *
+ * @author Res260
+ * @created_at 160022
+ * @modified_at 160022
+ */
+class CRUDConditionsParticulieres extends TestCase
+{
+	public function test1() {
+		$this->assertTrue(true);
+	}
+}
