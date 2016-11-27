38fa5a6304025a67957b0eac8c32ff86d2e70a6d
steveduquet <steve.duquet@outlook.com>
2016-11-27 15:10:42 -0500

    Ajout du test d'insertion dans la table disponibilites.

--- tests/DisponibilitesBenevoles.php
+++ tests/DisponibilitesBenevoles.php
@@ -4,15 +4,32 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+use App\Models\Disponibilite;
+
 class DisponibilitesBenevoles extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+	use DatabaseTransactions;
+	
+	/**
+	 * @test
+	 * Sauvegarder les disponibilités dans une BD
+	 */
+    public function sauvegarde_d_un_bon_modele()
     {
-        $this->assertTrue(true);
+    	$disponibilite = new Disponibilite;
+    	$disponibilite->benevole_id = "1";
+    	$disponibilite->title = 'test';
+    	$disponibilite->isAllDay = "0";
+    	$disponibilite->start = "2016-11-27 10:00:00";
+    	$disponibilite->end = "2016-11-27 14:00:00";
+    	$this->assertTrue($disponibilite->save());
+    	$this->seeInDatabase('disponibilites', [
+    			'id'=>$disponibilite->id,
+    			'benevole_id'=>$disponibilite->benevole_id,
+    			'title'=>$disponibilite->title,
+    			'isAllDay'=>$disponibilite->isAllDay,
+    			'start'=>$disponibilite->start,
+    			'end'=>$disponibilite->end
+    	]);
     }
 }
